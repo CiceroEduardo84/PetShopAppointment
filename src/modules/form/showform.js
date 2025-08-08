6bc33aa938bc74ba91bb664be4cf57a727f57494
Cicero Eduardo <ciceroeduardobarbosa@hotmail.com>
2025-08-07 23:50:03 -0300

    feat: add button close form appointment

--- src/modules/form/showform.js
+++ src/modules/form/showform.js
@@ -1,9 +1,15 @@
 const buttonAppointment = document.getElementById("showForm");
 const pageNewAppointment = document.querySelector(".containerNewAppointment");
 const boxNewAppointment = document.querySelector(".boxNewAppointment");
+const buttonClose = document.querySelector(".close");
+
 
 export const hideBoxAppointment = () => {
   pageNewAppointment.style.display = "none";
+};
+
+buttonClose.onclick = () => {
+    hideBoxAppointment();
 };
 
 // Show form
